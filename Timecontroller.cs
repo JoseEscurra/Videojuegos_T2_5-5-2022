42d694e616772d59a2207c426c09dec509079878
JoseEscurra <103553484+JoseEscurra@users.noreply.github.com>
2022-05-05 23:40:59 -0500

    Add files via upload

---
+++ Timecontroller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timecontroller : MonoBehaviour
+{
+    [SerializeField] int min, seg;
+    [SerializeField] Text tiempo;
+
+    private float restante;
+    private bool enMarcha;
+    
+    private void Awake() {
+        restante = (min * 60) + seg;
+        enMarcha = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enMarcha)
+        {
+            restante -= Time.deltaTime;
+            if(restante < 1)
+            {
+                enMarcha = false;
+            }
+            int tempMin = Mathf.FloorToInt(restante / 60);
+            int tempSeg = Mathf.FloorToInt(restante % 60);
+            tiempo.text = string.Format("{00:00}:{01:00}",tempMin,tempSeg);
+        }
+    }
+}
---
+++ ninja.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ninja : MonoBehaviour
+{
+    private Rigidbody2D m_rig;
+    public float speed;
+    public int vidas_n = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_rig.velocity = new Vector2(speed, m_rig.velocity.y);
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if(other.gameObject.tag == "plataforma")
+        {
+            speed *= -1;
+            this.transform.localScale = new Vector2(this.transform.localScale.x * -1, this.transform.localScale.y);
+        }    
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "bolaFuego")
+        {
+            vidas_n--;
+            if(vidas_n <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        if(other.gameObject.tag == "ataqueGrande")
+        {
+            vidas_n = 0;
+            Destroy(this.gameObject);
+        }
+
+    }
+}
